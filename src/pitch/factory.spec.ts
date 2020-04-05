867a9d7533ea1cd4030fb5dbc98db06bf3a65dba
Oscar Bouwmans <oscarbouwmans@me.com>
2020-04-05 09:43:32 +0200

    More Pitch Factory tests

--- src/pitch/factory.spec.ts
+++ src/pitch/factory.spec.ts
@@ -1,6 +1,28 @@
 import { createPitch } from "./factory";
 
 describe("Pitch Factory:", () => {
+    for (let naturalPosition = -1; naturalPosition < 6; naturalPosition += 1) {
+        const naturalName = (naturalPosition === -1 ? "f" : [ "c", "g", "d", "a", "e", "b" ][naturalPosition]);
+        describe(`${naturalName}:`, () => {
+            for (let accidentals = -3; accidentals <= 3; accidentals += 1) {
+                describe(`Accidentals ${accidentals}:`, () => {
+                    const circlePosition = naturalPosition + 7 * accidentals;
+                    for (let octave = -2; octave < 10; octave += 1) {
+                        describe(`Octave ${octave}:`, () => {
+                            const pitch = createPitch({ circlePosition, octave });
+                            it(`naturalName`, () => {
+                                expect(pitch.naturalName()).toEqual(naturalName);
+                            });
+                            it(`accidentals`, () => {
+                                expect(pitch.accidentals()).toEqual(accidentals);
+                            });
+                        });
+                    }
+                })
+            }
+        });
+    }
+
     describe("C Natural:", () => {
         const cNatural = createPitch({
             circlePosition: 0,
