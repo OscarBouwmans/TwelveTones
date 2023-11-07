3f62e51f96dd788dae3a25a87f5ed07eef3fa4bd
Oscar Bouwmans <oscarbouwmans@me.com>
2023-11-07 10:52:35 +0100

    Interval refactor: move old files to temp location

---
+++ src/pitch/interval/_trash/errors.ts
@@ -0,0 +1,15 @@
+// export const noIntervalData =
+//   "Unable to create Interval object: no data provided.";
+
+// export const invalidIntervalDefinition =
+//   "Unable to create Interval object from provided data.";
+
+// export const invalidIntervalDescription =
+//   "Unable to create IntervalDescription object from provided data.";
+
+// export const invalidIntervalName = "Invalid interval name given.";
+
+// export const invalidIntervalQuality = "Invalid interval quality given.";
+
+// export const invalidIntervalQualityFactor =
+//   "Invalid interval quality factor given.";
---
+++ src/pitch/interval/_trash/interval.ts
@@ -0,0 +1,18 @@
+// import { IntervalProperties } from "./properties";
+
+// /**
+//  * Barebones properties of an interval
+//  * @see Interval for extension with properties and methods
+//  */
+// export interface IntervalDefinition {
+//   circleShift: number;
+//   octaveShift: number;
+//   direction: IntervalDirection;
+// }
+
+// export interface Interval extends IntervalDefinition, IntervalProperties {}
+
+// export enum IntervalDirection {
+//   up = 1,
+//   down = -1,
+// }
